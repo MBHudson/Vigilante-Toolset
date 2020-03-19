11027f86047e7c72f42cda4ff9d26155a5386327
/game <VigilanteToolset@gmail.com>
2020-03-19 20:14:53 +0000

    Add files via upload

---
+++ ddosx4.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+terminator -e "/root/Vigilante-Toolset/ddos8000.sh" &
+terminator -e "/root/Vigilante-Toolset/ddos8000.sh" &
+terminator -e "/root/Vigilante-Toolset/ddos8000.sh" & 
+terminator -e "/root/Vigilante-Toolset/ddos8000.sh" 
+
+
---
+++ install.sh
@@ -0,0 +1,31 @@
+apt-get update
+apt-get install terminator -y
+apt-get install nikto -y
+apt-get install python3 -y  
+apt-get install python3-pip -y
+apt-get install sqlmap -y
+apt-get install uniscan -y
+apt-get install socat -y
+apt-get install hydra -y
+apt-get install tor -y
+apt-get install nmap -y
+apt-get install torbrowser-launcher -y
+apt-get install etherape -y
+apt install proxychains4 -y
+apt install docker
+apt install docker.io
+gcc ddos.c -o ddos
+gcc cDDoS.c -o cDDoS
+chmod +x proxy.sh
+chmod +x cproxy.sh
+chmod +x bruTOR.sh
+chmod +x cport.sh
+chmod +x ddos.sh
+chmod +x ddos8000.sh
+chmod +x ddosx4.sh
+chmod +x nmap.sh
+chmod +x proxy.sh
+chmod +x run.sh
+chmod +x sql.sh
+chmod +x Vigilante
+./Vigilante
